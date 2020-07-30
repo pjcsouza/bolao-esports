cf31e683515f0536d922996a0ba6c97172cf9c4a
cdbm <cdbm@cin.ufpe.br>
2020-07-30 20:27:04 -0300

    matches fetch

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, {useEffect}  from 'react';
+const axios = require('axios');
 
-// import { Container } from './styles';
-
+const access_token = 'NStrn8s0lmAn6EFOKn4jD4r8-VVmkf_FOAXlUIC3r1KJ4fIgI4I';
 function Home() {
+  var mt = localStorage.getItem("matches")
+
+  useEffect(()=>{
+    if(!mt){
+      mt = []
+      axios.get("https://api.pandascore.co/tournaments/4152",{
+        headers:{
+            "Authorization": `Bearer ${access_token}`
+        }
+      }).then(res=>{
+        res.data.matches.forEach(game =>{
+            const d = {
+                name : game.name,
+                begin_at : game.begin_at,
+                id : game.id,
+                live_embed_url : game.live_embed_url,
+                scheduled_at : game.scheduled_at,
+                status : game.status,
+                winner_id : game.winner_id
+            }
+            mt.push(d);
+        }) 
+        console.log(mt)
+        localStorage.setItem("matches", JSON.stringify(mt));
+      }).catch(err=>{
+        console.log(err)
+      })
+    }else{
+      console.log("matches already loaded")
+    }
+
+  },[]);
+
+
   return <h1>Home</h1>;
 }
 
