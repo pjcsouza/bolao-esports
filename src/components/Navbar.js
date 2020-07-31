3f9a83ddb341589cbed9b8232a16a3f1b872de53
unknown <pjcs@cin.ufpe.br>
2020-07-31 14:42:27 -0300

    navbar home

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,18 +12,12 @@ function NavbarHome() {
     <Nav className="mr-auto">
       <Nav.Link href="#home">Dados CBLOL</Nav.Link>
       
-      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
+      
     </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-success">Search</Button>
-    </Form>
+    <NavDropdown title="User" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Logout</NavDropdown.Item>
+        
+      </NavDropdown>
   </Navbar.Collapse>
 </Navbar>);
 }
